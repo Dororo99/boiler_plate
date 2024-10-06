f553b4d6a637ecd45643e9ac57811c6699b12a9a
Dororo99 <99dororo@gmail.com>
2024-10-06 21:27:07 +0900

    commit: login function

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const { User } = require('./models/User')
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
+
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser())
 
 mongoose.connect(config.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
@@ -27,8 +30,8 @@ app.post('/register', (req, res) => {
     // 그것들을 db에 넣어준다.
     const user = new User(req.body)
 
-    // save 전에 비밀번호 암호화
-    
+    // save 전에 비밀번호 암호화 
+    // User.js에서 bcrypt를 사용하여 암호화
     user.save()
         .then((userInfo) => {
             res.status(200).json({ success: true })
@@ -37,6 +40,41 @@ app.post('/register', (req, res) => {
             res.json({ success: false, err })
         })
 })
+
+// login 기능 구현
+app.post('/login', async (req, res) => {
+    try {
+        // 1. db안에서 요청된 이메일 찾기
+        const userInfo = await User.findOne({ email: req.body.email });
+        if (!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당되는 유저가 없습니다"
+            });
+        }
+        // 2. db에 있다면 비밀번호 일치 여부 확인
+        userInfo.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 일치하지 않습니다."
+                });
+            }
+            // 3. 비밀번호가 일치하면 토큰을 생성하기
+            userInfo.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // 토큰을 쿠키, 로컬 스토리지 등에 저장 가능
+                // 쿠키에 토큰 저장
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id });
+            });
+        });
+    } catch (err) {
+        return res.status(500).json({ loginSuccess: false, err });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -33,6 +34,7 @@ const userSchema = mongoose.Schema({
     }
 })
 
+// 비밀번호 암호화
 // 스키마.pre를 사용하면 모델이 저장되기 전에 실행되는 함수를 만들 수 있다.
 userSchema.pre('save', function (next) {
     var user = this
@@ -56,6 +58,39 @@ userSchema.pre('save', function (next) {
         next()
     }
 })
+
+// 비밀번호 비교
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // plainPassword를 암호화 하여 db에 저장된 암호화된 비밀번호와 비교
+    var user = this
+    bcrypt.compare(plainPassword, user.password, function (err, isMatch) {
+        if (err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+// 토큰 생성
+userSchema.methods.generateToken = function (cb) {
+    // jsonwebtoken을 이용하여 토큰 생성
+    var user = this
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    // user._id + 'secretToken' = token
+
+    user.token = token
+    return user.save()
+        .then(user => {
+            cb(null, user)
+        })
+        .catch(err => {
+            cb(err)
+        })
+
+    // user.save(function (err, user) {
+    //     if (err) return cb(err)
+    //     cb(null, user)
+    // })
+}
 
 const User = mongoose.model('User', userSchema)
 
